c85c1b538135e215f531f042a0d6d6f87d9ef4f4
Hiroshi Yamamoto <digiangler777@gmail.com>
2021-06-27 16:11:17 +0900

    Initial commit

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -255,7 +255,7 @@ source /usr/local/opt/asdf/asdf.fish
 
 
 # Flutter
-set -x PATH $HOME/develop/Front-end/Flutter/flutter/bin $PATH
+set -x PATH $HOME/development/Front-end/Flutter/flutter/bin $PATH
 
 # mysql
 
